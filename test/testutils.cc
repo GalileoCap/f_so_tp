920fd07105e8510c3f4d86d3fb73aa2b22a18724
GalileoCap <galileocapp@gmail.com>
2022-10-15 00:55:13 -0300

    FIX: Enter extra en config_parameters

--- test/testutils.cc
+++ test/testutils.cc
@@ -5,7 +5,7 @@ void createConfig(int ancho, int alto, int cantJugadores, struct Pos banderas[2]
   config << ancho << " " << alto << " " 
          << cantJugadores << std::endl
          << banderas[ROJO].x << " " << banderas[ROJO].y << std::endl
-         << banderas[AZUL].x << " " << banderas[AZUL].y << std::endl;
+         << banderas[AZUL].x << " " << banderas[AZUL].y;
   for (color equipo : {ROJO, AZUL})
     for (const struct Pos& pos : posiciones[equipo])
       config << std::endl << pos.x << " " << pos.y;
